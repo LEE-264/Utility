d2b43ed3b39707fe7cff228bb6b9aa8f1a9dd914
LEE-264 <stlyw36@gmail.com>
2025-06-09 17:43:06 +0900

    Update ArrList.h

--- CData/ArrList.h
+++ CData/ArrList.h
@@ -7,12 +7,12 @@ typedef struct {
     int* data;          // 실제 데이터를 담는 배열
     size_t size;        // 현재 저장된 원소 개수
     size_t capacity;    // 총 할당된 배열 크기
-} List;
+} ArrList;
 
-void list_init(List* list);
-void list_free(List* list);
-void list_push_back(List* list, int value);
-int  list_get(const List* list, size_t index);
-size_t list_size(const List* list);
+void arrlist_init(ArrList* list);
+void arrlist_free(ArrList* list);
+void arrlist_push_back(ArrList* list, int value);
+int  arrlist_get(const ArrList* list, size_t index);
+size_t arrlist_size(const ArrList* list);
 
-#endif // LIST_H
+#endif // ARRLIST_H
